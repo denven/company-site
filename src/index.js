e980f1a7cc9e630b9cd38cae0b0033c9a0b8377b
Chengwen <myregs@foxmail.com>
2020-04-30 08:36:55 -0700

    Add animation effect for glide slide caption

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-new Glide('.glide').mount();   // equivalent to the following 2 lines
-// const glide = new Glide('.glide');
-// glide.mount();
+// new Glide('.glide').mount();   // equivalent to the following 2 lines
+const glide = new Glide('.glide');
+
+// Add animation effect for each glide slide's caption
+glide.on(["mount.after", "run.after"], () => {
+ 
+  glide.on("run.before", () => {
+    document.querySelectorAll('.slide-caption > *').forEach(el => el.style.opacity = 0);
+  });
+
+  const captionsEl = document.querySelectorAll('.slide-caption');
+  const caption = captionsEl[glide.index];
+  anime({
+    targets: caption.children,  // 3 elements: h1/h3/button
+    opacity: [0, 1],
+    duration: 2000,             // total duration of all animations
+    easing: "linear",
+    delay: anime.stagger(400, {start: 300}), // intervals, show up one by one
+    translateY: [anime.stagger([40, 10]), 0],
+  });
+
+});
+
+glide.mount();
